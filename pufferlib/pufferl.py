cb15b5fb202ab7280e65ee90adcac627a5ae5e6d
Tim <ha24583@bristol.ac.uk>
2025-10-19 06:40:45 +0100

    added reward norm for ppo policy

--- pufferlib/pufferl.py
+++ pufferlib/pufferl.py
@@ -575,7 +575,8 @@ class PuffeRL:
             phi_encoded = self.phi_encoder(mb_obs)
             phi_encoded_tail = self.phi_encoder(mb_tail_obs).unsqueeze(1)
             phi_encoded = torch.cat([phi_encoded, phi_encoded_tail], dim =1)
-            mb_rewards = self._update_rewards_mb(phi_encoded,mb_skills).detach() ##CHECK THIS
+            mb_rewards = self._update_rewards_mb(phi_encoded,mb_skills).detach()
+            mb_rewards = (mb_rewards - mb_rewards.mean()) / (mb_rewards.std() + 1e-8)
 
             logits, newvalue = self.policy(mb_obs, state)
             actions, newlogprob, entropy = pufferlib.pytorch.sample_logits(logits, action=mb_actions)
